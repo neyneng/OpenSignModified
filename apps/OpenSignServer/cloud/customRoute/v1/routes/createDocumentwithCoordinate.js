f2aa91f19c3a00caa04e6fde74c761d3c8d1b353
vien.frivaldo <vien.frivaldo@acquirebpo.com>
2024-03-15 15:00:06 +0800

    change url for signature to public url instead of app server url

--- apps/OpenSignServer/cloud/customRoute/v1/routes/createDocumentwithCoordinate.js
+++ apps/OpenSignServer/cloud/customRoute/v1/routes/createDocumentwithCoordinate.js
@@ -60,7 +60,7 @@ export default async function createDocumentwithCoordinate(request, response) {
   const sendInOrder = request.body.sendInOrder || false;
   // console.log('fileData ', fileData);
   const protocol = customAPIurl();
-  const baseUrl = new URL(process.env.SERVER_URL);
+  const baseUrl = new URL(process.env.PUBLIC_URL);
   const reqToken = request.headers['x-api-token'];
   if (!reqToken) {
     return response.status(400).json({ error: 'Please Provide API Token' });
@@ -269,7 +269,7 @@ export default async function createDocumentwithCoordinate(request, response) {
 
               const objectId = contactMail[i].contactPtr.objectId;
               const hostUrl = baseUrl.origin;
-              let signPdf = `${process.env.PUBLIC_URL}/login/${res.id}/${contactMail[i].email}/${objectId}/${serverParams}`;
+              let signPdf = `${hostUrl}/login/${res.id}/${contactMail[i].email}/${objectId}/${serverParams}`;
               const openSignUrl = 'https://www.opensignlabs.com/contact-us';
               const orgName = parseExtUser.Company ? parseExtUser.Company : '';
               const themeBGcolor = '#47a3ad';
